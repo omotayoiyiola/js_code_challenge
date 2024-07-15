30e66d8a97c9c488e9738c30c5d54c9da814211a
Michofat <michofatltd@gmail.com>
2024-07-15 02:41:57 +0100

    capFirst Challenge

---
+++ easy/capFirst.js
@@ -0,0 +1,10 @@
+function capFirst(str) {
+  return str
+    .split(" ")
+    .map((word) => {
+      return word[0].toUpperCase() + word.slice(1);
+    })
+    .join(" ");
+}
+
+console.log(capFirst("i love you"));
